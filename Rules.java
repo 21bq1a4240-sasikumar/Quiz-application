597898476e71a092f625c131a00c28dcd1a3a05f
Sasi Kumar <113272089+21bq1a4240-sasikumar@users.noreply.github.com>
2026-01-05 10:19:28 +0530

    Add files via upload

---
+++ Rules.java
@@ -0,0 +1,72 @@
+package quiz.app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Rules extends JFrame implements ActionListener {
+
+    JButton start, back;
+    String name;
+
+    Rules(String name){
+
+        this.name = name;
+
+        JLabel heading = new JLabel("Welcome "+ name +" to QUIZ TEST");
+        heading.setBounds(250,100,750,30);
+        heading.setFont( new Font("Figtree", Font.BOLD, 28));
+        heading.setForeground(Color.WHITE);
+        add(heading);
+
+        JLabel rules = new JLabel();
+        rules.setBounds(70,150,700,350);
+        rules.setFont( new Font("Tahoma", Font.PLAIN, 16));
+        rules.setForeground(new Color(22,99,54));
+        
+        add(rules);
+
+        back = new JButton("Back");
+        back.setBounds(300,500,100,30);
+        back.setBackground(new Color(22,99,54));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+
+        start = new JButton("Start");
+        start.setBounds(450,500,100,30);
+        start.setBackground(new Color(22,99,54));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/rules.jpg"));
+        Image i = i1.getImage().getScaledInstance(800,650,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i);
+        JLabel image = new JLabel(i2);
+        image.setBounds(0,0,800,650);
+        add(image);
+
+        setSize(800,650);
+        setLocation(350,100);
+        setLayout(null);
+        setUndecorated(true);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == start){
+            setVisible(false);
+            new Quiz(name);
+        }else {
+            setVisible(false);
+            new Login();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
